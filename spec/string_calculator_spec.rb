7043d643f8bd25de9067ca07276fc878915d8f2c
Urvashi Vasita <urvashi.vasita@tntra.io>
2024-05-21 18:45:32 +0530

    Refactored: Added the comments in spec file for code clarity and added context

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,28 +2,43 @@
 
 require_relative '../string_calculator'
 
+# RSpec tests for the StringCalculator class
 describe StringCalculator do
-  it 'returns 0 if the string is blank' do
-    string = ''
-    result = StringCalculator.add(string)
-    expect(result).to eq(0)
+  context 'Test Case 1' do
+    # Test case for when the input string is empty
+    it 'returns 0 if the string is blank' do
+      string = ''
+      result = StringCalculator.add(string)
+      # Expect the result to be 0
+      expect(result).to eq(0)
+    end
+
+    # Test case for when the input string contains a single number
+    it 'returns the number itself if a single value is provided' do
+      string = '2'
+      result = StringCalculator.add(string)
+      # Expect the result to be the integer value of the string
+      expect(result).to eq(string.to_i)
+    end
+
+    # Test case for when the input string contains two numbers separated by a comma
+    it 'returns the sum of two numbers if they are comma-separated' do
+      # Define a string with two numbers separated by a comma
+      string = '2,3'
+      result = StringCalculator.add(string)
+      # Expect the result to be the sum of the two numbers
+      expect(result).to eq(5)
+    end
   end
 
-  it 'returns the number itself if a single value is provided' do
-    string = '2'
-    result = StringCalculator.add(string)
-    expect(result).to eq(string.to_i)
-  end
-
-  it 'returns the sum of two numbers if they are comma-separated' do
-    string = '2,3'
-    result = StringCalculator.add(string)
-    expect(result).to eq(5)
-  end
-
-  it 'returns the sum of comma-separatedn n numbers ' do
-    string = '2,3,4'
-    result = StringCalculator.add(string)
-    expect(result).to eq(9)
+  context 'Test Case 2' do
+    # Test case for when the input string contains two numbers separated by a comma
+    it 'returns the sum of comma-separatedn n numbers ' do
+      # Define a string with two numbers separated by a comma
+      string = '2,3,4'
+      result = StringCalculator.add(string)
+      # Expect the result to be the sum of the all numbers
+      expect(result).to eq(9)
+    end
   end
 end
