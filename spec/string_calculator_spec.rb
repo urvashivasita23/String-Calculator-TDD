063bdfa85b49f4293343a8106e1640c5f8e2c5a3
Urvashi Vasita <urvashi.vasita@tntra.io>
2024-05-21 19:54:25 +0530

    Refactored: 1. Added the multiple context , added better comments, refactored the class file with rubocop standards

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,82 +2,66 @@
 
 require_relative '../string_calculator'
 
-# RSpec tests for the StringCalculator class
 describe 'StringCalculator: Test Case 1' do
-  # Test case for when the input string is empty
-  it 'returns 0 if the string is blank' do
-    string = ''
-    result = StringCalculator.add(string)
-    # Expect the result to be 0
-    expect(result).to eq(0)
+  context 'with empty input' do
+    it 'returns 0' do
+      expect_calculator_to_return('', 0)
+    end
   end
 
-  # Test case for when the input string contains a single number
-  it 'returns the number itself if a single value is provided' do
-    string = '2'
-    result = StringCalculator.add(string)
-    # Expect the result to be the integer value of the string
-    expect(result).to eq(string.to_i)
+  context 'with single number input' do
+    it 'returns the number itself' do
+      expect_calculator_to_return('2', 2)
+    end
   end
 
-  # Test case for when the input string contains two numbers separated by a comma
-  it 'returns the sum of two numbers if they are comma-separated' do
-    # Define a string with two numbers separated by a comma
-    string = '2,3'
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the two numbers
-    expect(result).to eq(5)
+  context 'with two numbers separated by a comma' do
+    it 'returns the sum of the two numbers' do
+      expect_calculator_to_return('2,3', 5)
+    end
   end
 end
 
 describe 'StringCalculator: Test Case 2' do
-  # Test case for when the input string contains n numbers separated by a comma
-  it 'returns the sum of comma-separated n numbers ' do
-    # Define a string with n numbers separated by a comma
-    string = '2,3,4'
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the all numbers
-    expect(result).to eq(9)
+  context 'with multiple numbers separated by a comma' do
+    it 'returns the sum of all numbers' do
+      expect_calculator_to_return('2,3,4', 9)
+    end
   end
 end
 
 describe 'StringCalculator: Test Case 3' do
-  it 'Valid Input: \n allowed between number : return the sum ' do
-    string = '1\n,3,4'
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the all numbers and allow \n between number
-    expect(result).to eq(8)
+  context 'with \'\n\' allowed between numbers' do
+    it 'returns the sum of all numbers' do
+      expect_calculator_to_return('1\n,3,4', 8)
+    end
   end
 
-  it 'Invalid Input: \n allowed between number : return Not allowd' do
-    string = "1,\n"
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the all numbers and not allow \n between number
-    expect(result).to eq('Not Allowed')
+  context 'with invalid input' do
+    it 'returns Not Allowed if \',\\n\' sequence is present' do
+      expect_calculator_to_return("1,\n", 'Not Allowed')
+    end
   end
 end
 
 describe 'StringCalculator: Test Case 4' do
-  it 'Support different delimiters : return the sum ' do
-    string = "//;\n1;2"
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the all numbers and allow ; between number
-    expect(result).to eq(3)
-  end
-
-  it 'Support different delimiters : return the sum ' do
-    string = "//#\n1#2#3"
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the all numbers and allow # between number
-    expect(result).to eq(6)
+  context 'with different delimiters' do
+    it 'supports different delimiters and returns the sum' do
+      expect_calculator_to_return("//;\n1;2", 3)
+      expect_calculator_to_return("//#\n1#2#3", 6)
+    end
   end
 end
 
 describe 'StringCalculator: Test Case 5' do
-  it 'Negative Number not allowed : return error message : single value' do
-    string = '1, -2, 4'
-    result = StringCalculator.add(string)
-    # Expect the result to be the sum of the all numbers and allow \n between number
-    expect(result).to eq('negatives not allowed -2')
+  context 'with negavtive input' do
+    it 'raises an error if a negative number is provided' do
+      expect { StringCalculator.add('1, -2, 4') }.to raise_error(RuntimeError, 'negatives not allowed -2')
+    end
   end
+end
+
+def expect_calculator_to_return(input, expected_result)
+  result = StringCalculator.add(input)
+  expect(result).to eq(expected_result)
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -3,22 +3,21 @@
 # String Calculator File
 class StringCalculator
   def self.add(string_input)
-    if string_input.chars.count <= 1
-      string_input.empty? ? 0 : string_input.to_i
-    else
-      return 'Not Allowed' if string_input.include?(",\n")
+    return 0 if string_input.empty?
 
-      delimiter = ','
-      if string_input.start_with?('//')
-        delimiter = string_input[2]
-        string_input = string_input.split("\n", 2).last
-      end
+    delimiter = string_input.start_with?('//') ? string_input[2] : ','
+    string_input = string_input.split("\n", 2).last if string_input.start_with?('//')
 
-      numbers = string_input.split(delimiter).map(&:to_i)
-      negatives = numbers.select(&:negative?)
-      return "negatives not allowed #{negatives.join(' ')}" if negatives.any?
+    return 'Not Allowed' if string_input.include?(",\n")
 
-      numbers.sum
-    end
+    numbers = string_input.split(delimiter).map(&:to_i)
+
+    handle_negatives(numbers)
+    numbers.sum
+  end
+
+  def self.handle_negatives(numbers)
+    negatives = numbers.select(&:negative?)
+    raise "negatives not allowed #{negatives.join(' ')}" unless negatives.empty?
   end
 end
